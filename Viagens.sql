4e6a30ee58f5f75320bd953ef08033b125e1caad
Bruno Vieira Santos <163612337+bruunovsanttos@users.noreply.github.com>
2024-07-09 20:18:41 -0300

    Add files via upload
    banco de dados sql das aulas dio

---
+++ Viagens.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: mariadb-178069-db.mariadb-178069:10120
+-- Tempo de geração: 09-Jul-2024 às 23:16
+-- Versão do servidor: 11.1.2-MariaDB-1:11.1.2+maria~ubu2004
+-- versão do PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `Viagens`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `destinos`
+--
+
+CREATE TABLE `destinos` (
+  `id` int(11) DEFAULT NULL,
+  `nome` varchar(255) NOT NULL COMMENT 'nome do destino',
+  `descricao` varchar(255) NOT NULL COMMENT 'DEscricao destino'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `reservas`
+--
+
+CREATE TABLE `reservas` (
+  `id` int(11) DEFAULT NULL COMMENT 'identificador unico de reserva',
+  `id_usuario` int(11) DEFAULT NULL COMMENT 'referencia do id do usuario que fez a reserva',
+  `id_destino` int(11) DEFAULT NULL COMMENT 'referencia do id do destino da reserva',
+  `data` date DEFAULT NULL COMMENT 'data da reserva',
+  `status` varchar(255) DEFAULT 'pendente' COMMENT 'satatus da reserva (confirmada, pendente,cancelada, etc)'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) DEFAULT NULL,
+  `nome` varchar(255) NOT NULL COMMENT 'nome do usuario',
+  `email` varchar(100) NOT NULL COMMENT 'email do usuario',
+  `endereco` varchar(50) NOT NULL COMMENT 'Endereço do usuario',
+  `data_nascimento` date NOT NULL COMMENT 'Data Nascimento do usuario'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `destinos`
+--
+ALTER TABLE `destinos`
+  ADD UNIQUE KEY `nome` (`nome`);
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD UNIQUE KEY `email` (`email`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
