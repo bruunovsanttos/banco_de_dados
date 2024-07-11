10fd0af59ac8c25635267dccc53cf57c28c4fef6
Bruno Vieira Santos <163612337+bruunovsanttos@users.noreply.github.com>
2024-07-11 12:49:14 -0300

    Add files via upload
    Adição de chaves primarias e chaves estrangeiras no banco de dados. seguindo os comandos Alter Table e Add primary e foreing key na tabela.

--- Viagens.sql
+++ Viagens.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mariadb-178069-db.mariadb-178069:10120
--- Tempo de geração: 10-Jul-2024 às 15:43
+-- Tempo de geração: 11-Jul-2024 às 15:46
 -- Versão do servidor: 11.1.2-MariaDB-1:11.1.2+maria~ubu2004
 -- versão do PHP: 7.2.34
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `destinos` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL COMMENT 'nome do destino',
   `descricao` varchar(255) NOT NULL COMMENT 'DEscricao destino'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -39,10 +39,10 @@ CREATE TABLE `destinos` (
 --
 
 INSERT INTO `destinos` (`id`, `nome`, `descricao`) VALUES
-(3, 'Cachoeira do Vale Verde', 'Uma cachoeira exuberante cercada por natureza'),
-(4, 'Cidade Histórica de Pedra Alta', 'Uma cidade rica em história e arquitetura'),
+(1, 'Praia Grande', 'Praia do Povo'),
 (2, 'Praia das Tartarugas', 'Uma bela praia com areias brancas e mar cristalino'),
-(1, 'Praia Grande', 'Praia do Povo');
+(3, 'Cachoeira do Vale Verde', 'Uma cachoeira exuberante cercada por natureza'),
+(4, 'Cidade Histórica de Pedra Alta', 'Uma cidade rica em história e arquitetura');
 
 -- --------------------------------------------------------
 
@@ -51,7 +51,7 @@ INSERT INTO `destinos` (`id`, `nome`, `descricao`) VALUES
 --
 
 CREATE TABLE `reservas` (
-  `id` int(11) DEFAULT NULL COMMENT 'identificador unico de reserva',
+  `id` int(11) NOT NULL,
   `id_usuario` int(11) DEFAULT NULL COMMENT 'referencia do id do usuario que fez a reserva',
   `id_destino` int(11) DEFAULT NULL COMMENT 'referencia do id do destino da reserva',
   `data` date DEFAULT NULL COMMENT 'data da reserva',
@@ -63,10 +63,9 @@ CREATE TABLE `reservas` (
 --
 
 INSERT INTO `reservas` (`id`, `id_usuario`, `id_destino`, `data`, `status`) VALUES
-(1, 1, 1, '2023-11-11', 'pendente'),
-(2, 3, 2, '2023-07-10', 'confirmada'),
-(2, 2, 1, '2023-08-05', 'pendente'),
-(3, 3, 3, '2023-09-20', 'cancelada');
+(1, 3, NULL, '2024-11-11', 'pendente'),
+(2, 1, NULL, '2024-11-11', 'pendente'),
+(3, 3, NULL, '2024-11-11', 'pendente');
 
 -- --------------------------------------------------------
 
@@ -75,7 +74,7 @@ INSERT INTO `reservas` (`id`, `id_usuario`, `id_destino`, `data`, `status`) VALU
 --
 
 CREATE TABLE `usuarios` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL COMMENT 'nome do usuario',
   `email` varchar(100) NOT NULL COMMENT 'email do usuario',
   `endereco` varchar(150) DEFAULT NULL,
@@ -87,10 +86,10 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `endereco`, `data_nascimento`) VALUES
+(1, 'Bruno Santos', 'teste@teste.com', 'Av Novo Horizonte, 406 - Sacadura Santo André/SP', '1992-10-09'),
 (2, 'João Silva', 'joao@example.com', 'Rua A, 123, Cidade X, Estado Y', '1990-05-15'),
 (3, 'Maria Santos', 'maria@example.com', 'Rua B, 456, Cidade Y, Estado Z', '1985-08-22'),
-(4, 'Pedro Souza', 'pedro@example.com', 'Avenida C, 789, Cidade X, Estado Y', '1998-02-10'),
-(1, 'Bruno Santos', 'teste@teste.com', 'Av Novo Horizonte, 406 - Sacadura Santo André/SP', '1992-10-09');
+(4, 'Pedro Souza', 'pedro@example.com', 'Avenida C, 789, Cidade X, Estado Y', '1998-02-10');
 
 --
 -- Índices para tabelas despejadas
@@ -100,13 +99,56 @@ INSERT INTO `usuarios` (`id`, `nome`, `email`, `endereco`, `data_nascimento`) VA
 -- Índices para tabela `destinos`
 --
 ALTER TABLE `destinos`
+  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `nome` (`nome`);
+
+--
+-- Índices para tabela `reservas`
+--
+ALTER TABLE `reservas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_reservas_usuarios` (`id_usuario`),
+  ADD KEY `fk_reservas_destinos` (`id_destino`);
 
 --
 -- Índices para tabela `usuarios`
 --
 ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `destinos`
+--
+ALTER TABLE `destinos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `reservas`
+--
+ALTER TABLE `reservas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `reservas`
+--
+ALTER TABLE `reservas`
+  ADD CONSTRAINT `fk_reservas_destinos` FOREIGN KEY (`id_destino`) REFERENCES `destinos` (`id`),
+  ADD CONSTRAINT `fk_reservas_usuarios` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
